c1df1fea0653e9273a8e5312b41789a666e012b5
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-06 00:25:34 +0530

    [Ravina] Added: Editing the details of  a person

--- AddressBookSystem1/AddressBook.cs
+++ AddressBookSystem1/AddressBook.cs
@@ -114,9 +114,122 @@ namespace AddressBookSystem1
 
         }
 
+        public void EditDeatils()
+        {
+            if (AddressBook.Details.Count > 0)
+            {
+                Console.Write("Enter Name of the contact you want to edit: ");
+                string editDetails = Console.ReadLine();
 
+
+
+                foreach (var item in AddressBook.Details)
+                {
+                    if (editDetails.ToLower() == item.FirstName.ToLower())
+                    {
+                        Console.WriteLine("\nChoice what you want to edit\n 1.First name\n2.Last name\n3.Address" +
+                            "\n4.City\n5.State\n6.Zip Code\n7.Phone Number\n8.Email");
+
+                        int editChoice = int.Parse(Console.ReadLine());
+
+                        switch (editChoice)
+                        {
+                            case 1:
+                                Console.Write("Enter new First Name: ");
+                                item.FirstName = Console.ReadLine();
+                                break;
+
+                            case 2:
+                                Console.Write("Enter new Last Name: ");
+                                item.LastName = Console.ReadLine();
+                                break;
+
+                            case 3:
+                                Console.Write("Enter Your new Address: ");
+                                item.Address = Console.ReadLine();
+                                break;
+
+                            case 4:
+                                Console.Write("Enter Your new City Name: ");
+                                item.City = Console.ReadLine();
+                                break;
+
+                            case 5:
+                                Console.Write("Enter new State Name: ");
+                                item.State = Console.ReadLine();
+                                break;
+
+                            case 6:
+                                while (true)
+                                {
+                                    Console.Write("Enter new Zip Code: ");
+                                    string code = Console.ReadLine();
+
+                                    if (code.Length == 6)
+                                    {
+                                        item.ZipCode = code;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Enter a valid 6 digit Zip Code.");
+                                    }
+                                }
+                                break;
+
+                            case 7:
+                                while (true)
+                                {
+                                    Console.Write("Enter Your new Phone Number: ");
+                                    string Phonenumber = Console.ReadLine();
+
+                                    if (Phonenumber.Length == 10)
+                                    {
+                                        item.PhoneNumber = Phonenumber;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Enter a valid 10 digit Phone Number.");
+                                    }
+                                }
+                                break;
+
+                            case 8:
+                                while (true)
+                                {
+                                    Console.Write("Enter Your new Email Address: ");
+                                    string Email = Console.ReadLine();
+
+                                    if (Email.Contains("@"))
+                                    {
+                                        item.EmailId = Email;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Enter a valid Email Address.");
+                                    }
+                                }
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} does not exists in Address Book.", editDetails);
+                    }
+                }
+
+            }
+
+        }
     }
 }
+
+       
+    
+    
 
        
     
