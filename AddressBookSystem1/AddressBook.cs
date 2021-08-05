3c9d594de61053b9e824bd88fa93e06c4b470abb
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-06 00:18:26 +0530

    [Ravina] Added : Adding Multiple contacts

--- AddressBookSystem1/AddressBook.cs
+++ AddressBookSystem1/AddressBook.cs
@@ -9,78 +9,85 @@ namespace AddressBookSystem1
 
         public static List<Person> Details = new List<Person>();
 
+
         public static void EnterDetails()
         {
-            Person details = new Person();
-
-            Console.Write("Enter First name : ");
-            details.FirstName = Console.ReadLine();
-
-            Console.Write("Enter Last Name : ");
-            details.LastName = Console.ReadLine();
-
-            Console.Write("Enter Address : ");
-            details.Address = Console.ReadLine();
-
-            Console.Write("Enter City : ");
-            details.City = Console.ReadLine();
-
-            Console.Write("Enter State : ");
-            details.State = Console.ReadLine();
-
-            while (true)
+            Console.WriteLine("How many contacts do you want to add?");
+            int Count = Convert.ToInt32(Console.ReadLine());
+            while (Count > 0)
             {
-                Console.Write("Enter Zip Code  : ");
-                string code = Console.ReadLine();
 
-                if (code.Length == 6)
+                Person details = new Person();
+
+                Console.Write("Enter First name : ");
+                details.FirstName = Console.ReadLine();
+
+                Console.Write("Enter Last Name : ");
+                details.LastName = Console.ReadLine();
+
+                Console.Write("Enter Address : ");
+                details.Address = Console.ReadLine();
+
+                Console.Write("Enter City : ");
+                details.City = Console.ReadLine();
+
+                Console.Write("Enter State : ");
+                details.State = Console.ReadLine();
+
+                while (true)
                 {
-                    details.ZipCode = code;
-                    break;
+                    Console.Write("Enter Zip Code  : ");
+                    string code = Console.ReadLine();
+
+                    if (code.Length == 6)
+                    {
+                        details.ZipCode = code;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a valid 6 digit Zip Code.");
+                    }
                 }
-                else
+
+
+                while (true)
                 {
-                    Console.WriteLine("Enter a valid 6 digit Zip Code.");
+                    Console.Write("Enter Your Phone Number: ");
+                    string Phonenumber = Console.ReadLine();
+
+                    if (Phonenumber.Length == 10)
+                    {
+                        details.PhoneNumber = Phonenumber;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a valid 10 digit Phone Number.");
+                    }
                 }
+
+                while (true)
+                {
+                    Console.Write("Enter Your Email Address: ");
+                    string Email = Console.ReadLine();
+
+                    if (Email.Contains("@"))
+                    {
+                        details.EmailId = Email;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter a valid Email Address.");
+                    }
+                }
+
+                Details.Add(details);
+
+                Console.WriteLine("..................");
+                Count++;
             }
-
-
-            while (true)
-            {
-                Console.Write("Enter Your Phone Number: ");
-                string Phonenumber = Console.ReadLine();
-
-                if (Phonenumber.Length == 10)
-                {
-                    details.PhoneNumber = Phonenumber;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Enter a valid 10 digit Phone Number.");
-                }
-            }
-
-            while (true)
-            {
-                Console.Write("Enter Your Email Address: ");
-                string Email = Console.ReadLine();
-
-                if (Email.Contains("@"))
-                {
-                    details.EmailId = Email;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Enter a valid Email Address.");
-                }
-            }
-
-            Details.Add(details);
-
-            Console.WriteLine("..................");
-
         }
 
         public void ViewTheDetails()
@@ -113,6 +120,3 @@ namespace AddressBookSystem1
 
        
     
-
-    }
-}
