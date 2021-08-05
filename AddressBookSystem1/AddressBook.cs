8970b63e304d046d65c40fb732f4c1c1ac1e3210
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-05 23:53:13 +0530

    [Ravina] Added :Adding Details of person in address book

---
+++ AddressBookSystem1/AddressBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem1
+{
+    class AddressBook
+    {
+
+        Person person = new Person();
+    }
+}
