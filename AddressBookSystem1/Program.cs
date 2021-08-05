438b641a2be683c16e1143dab4701a18a0fa2c5a
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-06 00:47:46 +0530

    [Rvaina] Added : Main Program File

--- AddressBookSystem1/Program.cs
+++ AddressBookSystem1/Program.cs
@@ -11,7 +11,7 @@ namespace AddressBookSystem1
             while (true)
             {
                 Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
-                    " \n 3 for editing the existing contact");
+                    " \n 3 for editing the existing contact \n 4 for deleting the contact");
                 Console.WriteLine("................................................");
 
                 int choice = int.Parse(Console.ReadLine());
@@ -32,6 +32,12 @@ namespace AddressBookSystem1
                     case 3:
                         AddressBook obj2 = new AddressBook();
                         obj2.EditDeatils();
+                        break;
+
+
+                    case 4:
+                        AddressBook obj3 = new AddressBook();
+                        obj3.DeleteName();
                         break;
 
                     default:
