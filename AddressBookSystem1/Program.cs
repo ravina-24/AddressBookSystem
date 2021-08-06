7d7eb5cf0fd238b19a3a790b6ff738d418b0204b
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-06 23:53:33 +0530

    [Ravina] Added: Main Program File

--- AddressBookSystem1/Program.cs
+++ AddressBookSystem1/Program.cs
@@ -2,54 +2,59 @@
 
 namespace AddressBookSystem1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+       public  static void Main(string[] args)
         {
-                Console.WriteLine("Welcome to Address Book System");
-                Person person = new Person();
-                AddressBook.AddMember();
+            Console.WriteLine("Welcome to Address Book System");
+           
 
+            while (true)
+            {
+                Console.WriteLine("\n Enter your choice \n 1 for Adding AddessbookName \n 2 for adding address book name in th existing address book\n 2 for viewing Address book" +
+                    " \n 3 for editing the existing contact \n 4 for deleting the contact\n 5for viewing Address book ");
+                Console.WriteLine("................................................");
 
-                while (true)
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
                 {
-                    Console.WriteLine("\n Enter your choice \n 1 for Adding Contacts \n 2 for viewing Address book" +
-                        " \n 3 for editing the existing contact \n 4 for deleting the contact ");
-                    Console.WriteLine("................................................");
 
-                    int choice = int.Parse(Console.ReadLine());
+                    case 1:
+                        AddressBook.AddressBookNewNameValidator();
+                        break;
 
-                    switch (choice)
-                    {
-                        case 1:
-                            AddressBook.AddMember();
-                            break;
+                    case 2:
+                        AddressBook.AddressBookExistingNameValidator();
+                        break;
 
-                        case 2:
-                            AddressBook.ViewTheDetails();
-                            break;
+                    case 3:
+                        AddressBook.EditDetails();
+                        break;
 
-                        case 3:
-                            AddressBook.EditDeatils();
-                            break;
+                    case 4:
+                        AddressBook.DeleteName();
+                        break;
 
-                        case 4:
-                            AddressBook.DeleteName();
-                            break;
+                    case 5:
+                        AddressBook.ViewTheDetails();
+                        break;
 
-                        default:
-                            Console.WriteLine("Enter valid choice.");
-                            break;
-
-                    }
+                    default:
+                        Console.WriteLine("Enter valid choice.");
+                        break;
 
                 }
+
+            }
         }
     }
 }
 
+              
+               
+         
+      
 
 
-
-
-
+               
