b305fceb3ed8dc030d14d23a81629ab01f49185d
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-07 21:48:39 +0530

    [Ravina] Added:  Main Program File

--- AddressBookSystem1/Program.cs
+++ AddressBookSystem1/Program.cs
@@ -13,8 +13,9 @@ namespace AddressBookSystem1
             {
                 Console.WriteLine("\n Enter your choice \n 1 for Adding AddessbookName \n 2 for adding address book name in th existing address book" +
                     " \n 3 for editing the existing contact \n 4 for deleting the contact\n 5 for viewing Address book \n 6 for searching person by its state and city " +
-                     "\n 7 for sorting the address book by state , city and zip  alphabetically" );
-                
+                     " \n 7 for for sorting address book alphabetically" + "\n 8 for sorting the address book by state , city and zip  alphabetically");
+
+
                 Console.WriteLine("................................................");
 
                 int choice = int.Parse(Console.ReadLine());
@@ -47,6 +48,10 @@ namespace AddressBookSystem1
                         break;
 
                     case 7:
+                        AddressBook.SortEntriesAlphabetically();
+                        break;
+
+                    case 8:
                         AddressBook.SortByCityStateZip();
                         break;
 
